ab9120c875ae16636560969d403b6346aa96c211
jakob-a2k <jakub.a2k@gmail.com>
2022-09-03 16:55:07 +0200

    Stworzenie mozliwosci wpisania dwolnego roku. Polaczenie kodu i naprawienie ponownego wyboru - blad po wpisaniu znaku nowej lini

--- main.cpp
+++ main.cpp
@@ -1,80 +1,145 @@
 #include <iostream>
-#include <limits>
-#include <cstdlib>
 #include <stdio.h>
-#include <string>
-#include <cstdlib>
+#include <limits>
+//#include <stdlib.h>
 
 using namespace std;
 
-bool is_number;
-void choose_the_year();
-void check_the_year();
+int choose_the_year();
+int check_the_year(int number);
 void repeat_the_year();
-string year,last_four_numbers;
-int number_length,number;
+void correct_input();
 
 int main()
 {
     for(;;){
-        choose_the_year();
-        check_the_year();
+
+        int number = choose_the_year();
+
+        check_the_year(number);
         repeat_the_year();
+
+        cin.clear();
+        cin.ignore(numeric_limits < streamsize > ::max(),'\n');
+
     }
+
     return 0;
+}
+
+int choose_the_year( ){
+
+	cout << "\nWitaj! Podaj dowolny rok, a ja sprawdze czy jest on przestepny!" << endl;
+		int tab[ 4 ];
+		char current_sign;
+		current_sign = getchar();
+		if( current_sign == '\n' ){
+            cout << "Ten znak nie jest liczba!"<< endl;
+            exit (0);
+		}
+		else if( current_sign > '0' && current_sign <= '9' ){
+			tab[ 0 ] = current_sign - '0';
+		}
+		else{
+			cout << "Ten znak nie jest liczba!"<< endl;
+            exit (0);
+		}
+
+		current_sign = getchar();
+		if( current_sign == '\n' ){
+			return tab [ 0 ];
+		}
+		else if( current_sign >= '0' && current_sign <= '9' ){
+			tab[ 1 ] = current_sign - '0';
+		}
+		else{
+			cout << "Ten znak nie jest liczba!"<< endl;
+            exit (0);
+		}
+
+		current_sign = getchar();
+		if( current_sign == '\n' ){
+			return tab[ 0 ] * 10 + tab[ 1 ] ;
+		}
+		else if( current_sign >= '0' && current_sign <= '9' ){
+			tab[ 2 ] = current_sign - '0';
+		}
+		else{
+			cout << "Ten znak nie jest liczba!"<< endl;
+            exit (0);
+		}
+
+		current_sign = getchar();
+		if( current_sign == '\n' ){
+			return  tab[ 0 ] * 100 + tab[ 1 ] * 10 + tab[ 2 ];
+		}
+		else if( current_sign >= '0' && current_sign <= '9' ){
+			tab[ 3 ] = current_sign - '0';
+		}
+		else{
+			cout << "Ten znak nie jest liczba!"<< endl;
+            exit (0);
+		}
+
+		for( ;; ){
+				current_sign = getchar();
+			if( current_sign == '\n' ){
+				return tab[ 0 ] * 1000 + tab[ 1 ] * 100 + tab[ 2 ] * 10 + tab[ 3 ];
+		    }
+		    else if( current_sign >= '0' && current_sign <= '9' ){
+		    	tab[ 0 ] = tab[ 1 ];
+		    	tab[ 1 ] = tab[ 2 ];
+		    	tab[ 2 ] = tab[ 3 ];
+		    	tab[ 3 ] = current_sign - '0';
+		    }
+		    else{
+		    	cout << "Ten znak nie jest liczba!"<< endl;
+            exit (0);
+			}
+		}
 
 }
-void choose_the_year(){
 
-    do{
-        is_number = true;
-        cout << "\nWitaj! Podaj dowolny rok, a ja sprawdze czy jest on przestepny!" << endl;
-        cin >> year;
-        for(int i = 0; i < year.length(); i++){
-            if( !isdigit(year[ i ] ) )
-                is_number = false;
-        }
-        if( !is_number )
-            cout << "To nie jest liczba!" << endl;
-    }while( !is_number );
-
-    number_length = year.length();
-    last_four_numbers = year.substr(number_length-4,4);
-    }
-void check_the_year(){
-
-    number = atoi(last_four_numbers.c_str());
+int check_the_year(int number){
     if((number%400 == 0) || (number%4 == 0 && number%100 != 0)){
-            cout << "Ten rok jest przestepny!";
-        }
-            else{
-                cout << "Ten rok nie jest przestepny!";
-        }
+        cout << "Ten rok jest przestepny!";
     }
+    else{
+        cout << "Ten rok nie jest przestepny!";
+    }
+}
+
 void repeat_the_year(){
 
-        cout << endl;
-        cout << "\nCzy chcesz ponownie sprawdzic jakis rok ?";
-        cout << "\nWcisnij t aby ponownie wpisac rok!";
-        cout << "\nWcisnij n aby zakonczyc program!" << endl;
+    cout << endl;
+    cout << "\nCzy chcesz ponownie sprawdzic jakis rok ?";
+    cout << "\nWcisnij t aby ponownie wpisac rok!";
+    cout << "\nWcisnij n aby zakonczyc program!" << endl;
 
-        char repeat ;
-        cin >> repeat;
-        while(repeat != 't'){
-            if(repeat == 't');
-            else if(repeat =='n'){
-                exit(0);
-            }
-            else{
-                cout << "Nie ma takiej opcji!" << endl;
-                cin.clear();
-                cin.ignore(numeric_limits < streamsize > ::max(), '\n');
-                cout << " Sprobuj wybrac ponownie!" << endl;
-                cin >> repeat;
-            }
-
+    char repeat ;
+    repeat = getchar();
+    while(repeat != 't'){
+        if(repeat == '\n'){
+            cout << "Nie ma takiej opcji!" << endl;
+            //cin.clear();
+            //cin.ignore(numeric_limits < streamsize > ::max(), '\n');
+            cout << " Sprobuj wybrac ponownie!" << endl;
+            repeat = getchar();
         }
+        else if(repeat =='n'){
+            exit(0);
+        }
+        else{
+            cout << "Nie ma takiej opcji!" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits < streamsize > ::max(), '\n');
+            cout << " Sprobuj wybrac ponownie!" << endl;
+            repeat = getchar();
+        }
+
+    }
 }
+
 
 
 
